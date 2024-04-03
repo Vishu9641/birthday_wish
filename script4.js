2f2248465f1e14c092ac0df7a94703166efe8910
Vishu9641 <93582305+Vishu9641@users.noreply.github.com>
2024-04-03 14:34:32 +0530

    script4.js

--- script4.js
+++ script4.js
@@ -1,5 +1,4 @@
 document.getElementById('playButton').addEventListener('click', function() {
-  // Replace 'song.mp3' with your song file
   var audio = new Audio('message.mp3');
   audio.play();
 });
