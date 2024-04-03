878b91138ec0d5aead30058c5abb9cd54e1708fb
Vishu9641 <93582305+Vishu9641@users.noreply.github.com>
2024-04-03 07:27:47 +0530

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,6 @@
+document.getElementById('playButton').addEventListener('click', function() {
+  // Replace 'song.mp3' with your song file
+  var audio = new Audio('Shinchan.mp3');
+  audio.play();
+});
+
